cc2031f77f8266a72ca68ecb77d49541ac4219a4
Giriraj <girirajcodingprofile@gmail.com>
2026-01-28 23:56:54 +0530

    Add files via upload

---
+++ JS/The_DOM.js
@@ -0,0 +1,32 @@
+// what is DOM ? How does it represent the HTML structure.
+// Name the types of nodes in the DOM structure
+// What is the difference bw an element, node and a text node
+// Inspect the following HTML in the browser and identify each node
+// What is the difference bw getElementById and querySelector
+// what does getElementsByClassName return? Is it an array
+// Use queryselectorAll to select all the buttons with class ".buy-now"
+// Select the heading of a page ID and change its text to "Welcome to sheriyans"
+// Select all the li elements and print their text using a loop
+// What is the difference bw innerText, textContent , and innerHTML
+// When should you use textContent instead of innerText
+// Select a paragraph and replace its content with: <b>Updates</b> by javascript
+// How do you get the src of an image using javascript
+// What does setAttribute do
+// Select a link and update its href to point to another website
+// Add a title attribute to a div dynamically
+// Remove the disabled attribute from a button 
+// What does createElement() do What's returned
+// What is the difference bw appendChild() and prepend()
+// can u remove an element using removeChild()
+
+
+// create a new list item <li> New Task </li> and add it to the end of ul
+// create a new image element with a placeholder source and add it to the top of div
+// Select the first item in a list and delete it from the DOM.
+// How do you change the background color of an element
+// What is the difference bw .classList.add() and .classList.toggle();
+
+// Add a highlight class to every even item in a list
+// Toggle a class active on a button when clicked (hint use: classList.toggle())
+// set the font size of all <p> elements to 18px using style
+// clasList vs style property
